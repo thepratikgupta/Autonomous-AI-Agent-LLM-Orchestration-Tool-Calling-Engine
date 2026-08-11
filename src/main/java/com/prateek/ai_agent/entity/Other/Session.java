5740a7acfff9dcf720cd2e53a3194f129a9cc210
Prateek Gupta <140331841+thepratikgupta@users.noreply.github.com>
2026-08-11 20:57:03 +0530

    Update Session.java

--- src/main/java/com/prateek/ai_agent/entity/Other/Session.java
+++ src/main/java/com/prateek/ai_agent/entity/Other/Session.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Document(collection = "sessions")//creates MongoDB collection
+@Document(collection = "sessions")
 @Getter
 @Setter
 @NoArgsConstructor
