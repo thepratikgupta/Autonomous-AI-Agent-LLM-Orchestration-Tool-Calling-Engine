d5f4e9d54795262c4f00ef0bc70fd422249075be
Prateek Gupta <140331841+thepratikgupta@users.noreply.github.com>
2026-08-11 20:51:23 +0530

    Refactor adminId retrieval in approve and reject methods

--- src/main/java/com/prateek/ai_agent/controller/CommandApprovalController.java
+++ src/main/java/com/prateek/ai_agent/controller/CommandApprovalController.java
@@ -17,7 +17,6 @@ public class CommandApprovalController {
     private final AuditorAwareImpl auditorAwareImpl;
 
     // GET MY COMMANDS
-
     @GetMapping("/my")
     public List<CommandRequest> getMyCommands() {
 
@@ -27,41 +26,31 @@ public class CommandApprovalController {
     }
 
     // GET ALL PENDING COMMANDS OF ALL USERS
-
     @GetMapping("/pending")
     public List<CommandRequest> getPendingCommands() {
         return commandApprovalService.getPendingCommands();
     }
 
     // APPROVE A COMMAND
-
     @PostMapping("/{id}/approve")
     public CommandRequest approveCommand(@PathVariable String id) {
-
-        String adminId = auditorAwareImpl.getCurrentAuditor()
-                .orElse("Guest User");
+        String adminId = auditorAwareImpl.getCurrentAuditor().orElse("Guest User");
         return commandApprovalService.approveCommand(id, adminId);
     }
 
     // REJECT A COMMAND
-
     @PostMapping("/{id}/reject")
     public CommandRequest rejectCommand(@PathVariable String id, @RequestBody Map<String, String> body) {
 
-        // adminId = String.valueOf(auditorAwareImpl.getCurrentAuditor());
-        String adminId = auditorAwareImpl.getCurrentAuditor()
-                .orElse("Guest User");
+        String adminId = auditorAwareImpl.getCurrentAuditor().orElse("Guest User");
         return commandApprovalService.rejectCommand(
                 id,
                 adminId,
                 body.getOrDefault("reason", "Rejected")
         );
-        //admin will send request like this : POST /commands/abc123/reject
-        //admin will also send JSON body : {"reason": "Dangerous filesystem access"}
     }
 
     // GET SINGLE COMMAND
-
     @GetMapping("/{id}")
     public CommandRequest getCommand(@PathVariable String id){
         return commandApprovalService.getCommandById(id);
