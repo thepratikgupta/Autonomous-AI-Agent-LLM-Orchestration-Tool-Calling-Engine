2e181ea720f67b80d558c5c0343d3c6d358aecd8
Prateek Gupta <140331841+thepratikgupta@users.noreply.github.com>
2026-08-11 21:04:27 +0530

    Update JwtAuthFilter.java

--- src/main/java/com/prateek/ai_agent/service/Authentication/JwtAuthFilter.java
+++ src/main/java/com/prateek/ai_agent/service/Authentication/JwtAuthFilter.java
@@ -33,27 +33,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         try {
             log.info("========== JWT FILTER START ==========");
             log.info("URI: {}", request.getRequestURI());
-            //log.info("incoming request: {}", request.getRequestURI());
 
             final String requestTokenHeader = request.getHeader("Authorization");
-            //for requests that do not need token like signup page, login page, swagger ui page
+            
             if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")) {
                 log.info("No Bearer token -> continuing filter chain");
-                filterChain.doFilter(request, response);//so that request,response can be use by other filters of filter chain
+                filterChain.doFilter(request, response);
                 log.info("Returned from filter chain");
                 log.info("Response status: {}", response.getStatus());
                 return;
             }
-            //HttpServletRequest request has Authorization header and it has token
+            
             log.info("Bearer token found");
             String token = requestTokenHeader.split("Bearer ")[1];
             log.info("Extracted JWT");
             String userId = jwtService.getUserIdFromAccessToken(token);
             log.info("JWT userId = {}", userId);
 
-            //TRUE ONLY WHEN : Valid JWT is present AND Request is NOT already authenticated
-            //If the request contains a valid JWT and no authentication has been set yet for this request,
-            //then authenticate the user and store them in Spring Security context.
             if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 log.info("No existing authentication -> loading user");
                 User user = userService.getUserById(userId);
@@ -72,8 +68,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
             log.info("========== AFTER FILTER CHAIN ==========");
             log.info("Response status: {}", response.getStatus());
-            //filterChain.doFilter(request, response);
-            //log.info("Response status :{}", response.getStatus());
+ 
         } catch (Exception ex) {
             log.error("========== EXCEPTION IN JWT FILTER ==========", ex);
             handlerExceptionResolver.resolveException(request, response, null, ex);
