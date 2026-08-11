4cbdfba860e3c0a2ae726a41699743160067bb47
Prateek Gupta <140331841+thepratikgupta@users.noreply.github.com>
2026-08-11 21:08:10 +0530

    Update MemoryAccessService.java

--- src/main/java/com/prateek/ai_agent/service/MemoryService/MemoryAccessService.java
+++ src/main/java/com/prateek/ai_agent/service/MemoryService/MemoryAccessService.java
@@ -1,10 +1,1 @@
-package com.prateek.ai_agent.service.MemoryService;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class MemoryAccessService {
-
-}
